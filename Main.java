a81d95a0cbe91154f25a96d7edd840c8298a0b03
SheikhGalib <galibmahim01@gmail.com>
2025-09-21 16:31:10 +0600

    merge conflict from feature

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main {
     // simulating multiple students accessing ebooks with lazy loading
     public static void main(String[] args) {
+        EBook ebook4 = new RealEbook("100 days of Machine learning", "6MB");
         EBook ebook1 = new RealEbook("Head First Java", "5MB");
         EBook ebook2 = new RealEbook("Design Patterns", "3MB");
         EBook ebook3 = new RealEbook("C++ the complete reference", "4MB");
-        EBook ebook4 = new RealEbook("100 days of Machine learning", "6MB");
 
         // Display simulation
+        ebook4.display();
         ebook1.display();
         ebook2.display();
         ebook3.display();
-        ebook4.display();
     }
 }
 
